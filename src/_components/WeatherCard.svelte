57fe3d2dc57a71ad9db1e5f02ec78b152ab11230
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-12-21 18:34:26 -0600

    add some QOL and component folder

---
+++ src/_components/WeatherCard.svelte
@@ -0,0 +1,23 @@
+<script>
+    import '../styles.css';
+    export let data;
+    export let expand = true;
+</script>
+
+<article class="weatherCard">
+    <button 
+        on:click={() => expand = !expand}
+        class="w-full"
+    >
+        <h2>Today's forecast</h2>
+    </button>
+    <hr />
+    {#if expand }
+        <br />
+        <div class="grid grid-cols-3 gap-2 justify-items-center">
+            <h3>{data.temperature}&deg;{data.temperatureUnit}</h3>
+            <h3>{data.detailedForecast}</h3>
+            <img src={data.icon} alt="forecast icon">
+        </div>
+    {/if}
+</article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
-    // import '../styles.css';
+	import '../styles.css';
+	import  WeatherCard from '../_components/WeatherCard.svelte';
+	let DEBUGGING = true;
 	// const forecastUrl = 'https://api.weather.gov/gridpoints/DMX/66,48/forecast';
 	// docs for weather.gov API https://www.weather.gov/documentation/services-web-api
 	let city = 'West Des Moines';
@@ -23,53 +25,61 @@
 	}
 
 	async function getGridInfo() {
-        try {
-            const geoIPResponse = await fetch(
-                // won't work due to some weird CORS thing running locally with http
-                // may work once deployed...
-                'https://ipapi.co/json/',
-            );
-            if (geoIPResponse.status === 200) {
-                const data = await geoIPResponse.json();
-                lat = data.latitude;
-                lng = data.longitude;
-            }
-        } catch (error) {
-            // fails for local development so we need to catch it for now untill
-            // we can figure out a better solution
+		// try {
+		// 	if (!DEBUGGING) {
+		// 		const geoIPResponse = await fetch(
+		// 			// won't work due to some weird CORS thing running locally with http
+		// 			// may work once deployed...
+		// 			'https://ipapi.co/json/'
+		// 		);
+		// 		if (geoIPResponse.status === 200) {
+		// 			const data = await geoIPResponse.json();
+		// 			lat = data.latitude;
+		// 			lng = data.longitude;
+		// 		}
+		// 	}
+		// } catch (error) {
+		// 	// we need to catch the CORS error for now untill we can figure out a better solution
+		// 	// keep this incase we need to use a custom API for geoip stuff
+		// 	// if (!lat || !lng) {
+		// 	// 	const customGeoIPResponse = await fetch('/geolocate.json').then((response) =>
+		// 	// 		response.json()
+		// 	// 	);
+		// 	// 	lat = customGeoIPResponse.latitude;
+		// 	// 	lng = customGeoIPResponse.longitude;
+		// 	// }
+		// }
 
-            // keep this incase we need to use a custom API for geoip stuff
-            // if (!lat || !lng) {
-            // 	const customGeoIPResponse = await fetch('/geolocate.json').then((response) =>
-            // 		response.json()
-            // 	);
-            // 	lat = customGeoIPResponse.latitude;
-            // 	lng = customGeoIPResponse.longitude;
-            // }
-        }
-
-		const pointsResponse = await fetch(pointsUrl()).then((response) => response.json());
-        // const pointsResponse = await fetch('/samplepointsWDM.json').then((response) => response.json());
-        // console.log(pointsResponse);
-        office = pointsResponse.properties.gridId;
-        gridX = pointsResponse.properties.gridX;
-        gridY = pointsResponse.properties.gridY;
-        city = pointsResponse.properties.relativeLocation.properties.city;
-        state = pointsResponse.properties.relativeLocation.properties.state;
+		let pointsResponse;
+		if (DEBUGGING) {
+			pointsResponse = await fetch('/samplepointsWDM.json').then((response) => response.json());
+		} else {
+			pointsResponse = await fetch(pointsUrl()).then((response) => response.json());
+		}
+		// console.log(pointsResponse);
+		office = pointsResponse.properties.gridId;
+		gridX = pointsResponse.properties.gridX;
+		gridY = pointsResponse.properties.gridY;
+		city = pointsResponse.properties.relativeLocation.properties.city;
+		state = pointsResponse.properties.relativeLocation.properties.state;
 	}
 
 	async function getForecast() {
 		const TONIGHT = 'Tonight';
 		await getGridInfo(); // need to set up office and grid data first
-		const response = await fetch(forecastUrl()).then((response) => response.json());
-		// const response = await fetch('/sampleForecastNighttime.json').then((response) => response.json());
-        todaysForecast.now = response.properties.periods[0];
-		// console.log(response);
-		if (todaysForecast.now.name === TONIGHT) {
-			todaysForecast.now = response.properties.periods[0];
+		let forecastResponse;
+		if (DEBUGGING) {
+			forecastResponse = await fetch('/sampleForecastNighttime.json').then((response) => response.json());
 		} else {
-			todaysForecast.now = response.properties.periods[0];
-			todaysForecast.tonight = response.properties.periods[1];
+			forecastResponse = await fetch(forecastUrl()).then((response) => response.json());
+		}
+		// console.log(response);
+		if (forecastResponse.properties.periods[0].name === TONIGHT) {
+			// we only have 1 item to display for today
+			todaysForecast.now = forecastResponse.properties.periods[0];
+		} else {
+			todaysForecast.now = forecastResponse.properties.periods[0];
+			todaysForecast.tonight = forecastResponse.properties.periods[1];
 		}
 	}
 </script>
@@ -78,13 +88,13 @@
 	<title>Your fun and honest Weather forecast</title>
 </svelte:head>
 
-<body class="prose">
-<h1>Welcome to a special weather App. It tells you the weather but is fun and honest about it</h1>
-<button on:click={getForecast}>Get forecast</button>
-{#if todaysForecast.now}
-    <h2>Today's forecast for {city}, {state}</h2>
-	{#each Object.entries(todaysForecast.now) as [key, value]}
-		<h2>{key} : {value}</h2>
-	{/each}
-{/if}
-</body>
+<div class="mx-auto bg-gray-900 text-gray-100 flex flex-col mx-auto text-center">
+	<h1>Welcome to a special weather App. It tells you the weather but is fun and honest about it</h1>
+	<button on:click={getForecast} class="purplebutton w-1/3 mx-auto">Get forecast</button>
+	{#if city && state}
+		<h2>{city}, {state}</h2>
+	{/if}
+	{#if todaysForecast.now}
+		<WeatherCard data={todaysForecast.now} />
+	{/if}
+</div>
