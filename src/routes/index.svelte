90a3af5fbfa90a3ea77e616d24845e2ff5364373
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2022-01-15 21:22:38 -0600

    try initializePage in server render?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
   // docs for weather.gov API https://www.weather.gov/documentation/services-web-api
 
   import { onMount } from 'svelte';
+import { init } from 'svelte/internal';
   import { fade } from 'svelte/transition';
   import '../styles.css';
   import WeatherCard from '../_components/WeatherCard.svelte';
@@ -21,10 +22,11 @@
   let forecast = [];
   let address = '';
   let displayForecast = false;
+  initializePage();
 
-  onMount(async () => {
-    await initializePage();
-  });
+  // onMount(async () => {
+  //   await initializePage();
+  // });
 
   function initializePage() {
     // asks for permission to get user's location
