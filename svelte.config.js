fc558f31dcdbb184ba6a2dfd1396995184f38850
Pachwenko <me@patrickmccartney.dev>
2022-05-02 12:20:26 -0600

    fix svelte-kit static adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,16 @@ export default {
 	],
 	kit: {
 		adapter: adapter({
-			// default options
+			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
 			precompress: false
-		})
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
